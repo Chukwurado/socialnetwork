87234baaa1eb969b034bbf615c9030e36bf3f59b
Chukwurado98 <chukwurado.umeaka98@gmail.com>
2019-06-29 17:18:17 -0400

    Change title

--- register.php
+++ register.php
@@ -12,7 +12,7 @@ require 'includes/form_handlers/login_handler.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>BB Beauties</title>
+    <title>Bi-Curious Beauties</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
@@ -27,7 +27,7 @@ require 'includes/form_handlers/login_handler.php';
 
         <div class="login_box">
             <div class="login_header">
-                <h1>BBeauties</h1>
+                <h1>Bi-Curious Beauties</h1>
                 Login or Sign up
             </div>
             <div id="first">
